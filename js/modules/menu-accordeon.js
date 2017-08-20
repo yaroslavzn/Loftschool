bf3fd7aff3cfd351dd5ad34437643794ef8e9a9b
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2017-08-20 15:10:24 +0300

    added menu accordeon

---
+++ js/modules/menu-accordeon.js
@@ -0,0 +1,33 @@
+$(function() {
+	$('.menu__item-title').on('click', function(e) {
+		
+		e.preventDefault();
+
+		var element = $(e.target);
+		var item = element.closest('.menu__item');
+		var items = item.siblings();
+		var itemsContent = items.find('.menu__item-text-wrap');
+		var content = item.find('.menu__item-text-wrap');
+		var reqWidth = content.find('.menu__item-text').outerWidth();
+
+		if(!item.hasClass('menu__item--active')) {
+			items.removeClass('menu__item--active');
+			item.addClass('menu__item--active');
+
+			itemsContent.css({
+				'width': 0
+			})
+
+			content.css({
+				'width': reqWidth
+			})
+		} else {
+
+			item.removeClass('menu__item--active');
+
+			content.css({
+				'width': 0
+			})
+		}
+	});
+});
